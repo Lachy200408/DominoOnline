c5d35faf06531b151f729ee06ead6fb7d32a7cb9
ReyGF <gomezreynol14@gmail.com>
2024-05-28 04:36:02 +0200

    js -> inizializar_fichas

---
+++ web/dominologic/player.js
@@ -0,0 +1,18 @@
+class Player{
+    constructor(fichas)
+    {
+        this.fichas = fichas;
+    }
+    AAAAAA()
+    {
+        console.log("aaaaaaaa")
+    }
+    
+}
+
+function Saludar()
+{
+    console.log("Hola desde player")
+}
+export { Saludar }
+export { Player }
